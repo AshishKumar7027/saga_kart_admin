d6e8115786bc81191741438a609c5cec0721f496
Ashish <ashuusheoran@gmail.com>
2024-12-04 23:11:32 +0530

    cart provider & cart service added

---
+++ lib/cart/provider/cart_provider.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:saga_kart_admin/cart/model/cart_model.dart';
+import 'package:saga_kart_admin/cart/model/cart_response.dart';
+import 'package:saga_kart_admin/cart/service/cart_service.dart';
+import 'package:saga_kart_admin/core/app_util.dart';
+
+class CartProvider extends ChangeNotifier {
+  CartProvider(this.cartService);
+
+  final CartService cartService;
+  bool isLoading=false;
+  String? error;
+  CartResponse? cartResponse;
+
+  Future fetchCartItems()async{
+    try{
+      error = null;
+      isLoading = true;
+       // notifyListeners();
+      cartResponse = await cartService.fetchCartItems();
+      isLoading = false;
+    }catch(e){
+      isLoading = false;
+      error=e.toString();
+      AppUtil.showToast(error!);
+    }
+    notifyListeners();
+  }
+  Future addToCart(CartModel cartModel) async {
+    try {
+      error=null;
+      isLoading=true;
+      notifyListeners();
+     final success = await cartService.addToCart(cartModel);
+     if(success){
+       AppUtil.showToast('Item Added To Cart');
+       fetchCartItems();
+     }
+     isLoading=false;
+    } catch (e) {
+      isLoading=false;
+      error=e.toString();
+      AppUtil.showToast(error!);
+    }
+    notifyListeners();
+  }
+
+
+  Future updateCartQuantity(CartModel cartModel) async {
+    try {
+      error=null;
+      isLoading=true;
+      notifyListeners();
+      final success = await cartService.updateCartQuantity(cartModel);
+      if(success){
+        AppUtil.showToast('${cartModel.quantity}  Quantity updated');
+      }
+      isLoading=false;
+    } catch (e) {
+      isLoading=false;
+      error=e.toString();
+      AppUtil.showToast(error!);
+    }
+    notifyListeners();
+  }
+  Future deleteCartItem(String id)async{
+    try{
+      error=null;
+      isLoading=true;
+      notifyListeners();
+      bool isSuccess =await  cartService.deleteCartItem(id);
+      isLoading=false;
+      if(isSuccess){
+        AppUtil.showToast('Cart item deleted successfully');
+      }
+
+    }catch(e){
+      error=e.toString();
+    }
+    notifyListeners();
+  }
+}
---
+++ lib/cart/service/cart_service.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:saga_kart_admin/cart/model/cart_model.dart';
+import 'package:saga_kart_admin/cart/model/cart_response.dart';
+import 'package:saga_kart_admin/core/api_endpoint.dart';
+
+class CartService {
+  Future addToCart(CartModel cartModel) async {
+    String url = ApiEndpoint.cart;
+    final header = await ApiEndpoint.getHeader();
+    final response = await http.post(
+      Uri.parse(url),
+      body: jsonEncode(
+        cartModel.toJson(),
+      ),
+      headers: header,
+    );
+    if (response.statusCode == 201) {
+      return true;
+    } else {
+      throw 'product add nhi hua hai cart me';
+    }
+  }
+
+  Future<CartResponse> fetchCartItems() async {
+    String url = ApiEndpoint.cart;
+    final header = await ApiEndpoint.getHeader();
+    final response = await http.get(Uri.parse(url), headers: header);
+    if (response.statusCode == 200){
+      String body = response.body;
+      final json = jsonDecode(body);
+      return CartResponse.fromJson(json);
+    } else {
+      throw 'Unable to fetch cart Item';
+    }
+  }
+  Future<bool> updateCartQuantity(CartModel cartModel)async{
+    String url = ApiEndpoint.cart;
+    final header = await ApiEndpoint.getHeader();
+    final response = await http.put(Uri.parse(url),
+      headers: header,
+      body: jsonEncode(cartModel.toJson(),
+      ),
+    );
+    if(response.statusCode == 200){
+      return true;
+    }
+    throw 'Unable to update cart Quantity';
+
+  }
+  Future<bool> deleteCartItem(String id) async {
+    Uri uri = Uri.parse(ApiEndpoint.deleteCartItem(id));
+    final header = await ApiEndpoint.getHeader();
+    Response response = await http.delete(uri, headers: header);
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      throw 'Unable to delete cart item';
+    }
+  }
+}
